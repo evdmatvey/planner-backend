dc0fcdc798f696a3efa703c7fd90f2e56e4f7878
evdmatvey <evd.matvey@mail.ru>
2025-01-06 23:57:59 +0700

    Add prisma service

---
+++ src/shared/mocks/prisma-service.mock.ts
@@ -0,0 +1,10 @@
+export const mockPrismaService = {
+  user: {
+    findUnique: jest.fn(),
+    create: jest.fn(),
+    findMany: jest.fn(),
+    findFirst: jest.fn(),
+    delete: jest.fn(),
+    update: jest.fn(),
+  },
+};
