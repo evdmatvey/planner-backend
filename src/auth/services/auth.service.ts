012fc6e6f5ac1036ee704ff37195c17e38c0bf2f
evdmatvey <evd.matvey@mail.ru>
2025-01-07 16:28:15 +0700

    Add auth service

---
+++ src/auth/services/auth.service.ts
@@ -0,0 +1,69 @@
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { User } from '@prisma/__generated__';
+import { verify } from 'argon2';
+import { UserService } from '@/user';
+import { AuthMessageConstants } from '../constants/auth-message.constants';
+import { LoginDto } from '../dto/login.dto';
+import { RegisterDto } from '../dto/register.dto';
+import { AuthResult } from '../types';
+
+@Injectable()
+export class AuthService {
+  public constructor(private readonly _userService: UserService) {}
+
+  public async login(dto: LoginDto): Promise<AuthResult> {
+    const user = await this._validateUser(dto);
+
+    return this._getUserWithoutPassword(user);
+  }
+
+  public async register(dto: RegisterDto): Promise<AuthResult> {
+    const isUserExist = await this._userService.getByEmail(dto.email);
+
+    if (isUserExist)
+      throw new ConflictException(AuthMessageConstants.USER_ALREADY_EXIST);
+
+    const { passwordRepeat, ...createUserDto } = dto;
+
+    const user = await this._userService.create(createUserDto);
+
+    return this._getUserWithoutPassword(user);
+  }
+
+  public async getUserById(id: string): Promise<AuthResult> {
+    const user = await this._userService.getById(id);
+
+    if (!user) throw new NotFoundException(AuthMessageConstants.USER_NOT_FOUND);
+
+    return this._getUserWithoutPassword(user);
+  }
+
+  private async _validateUser(dto: LoginDto): Promise<User> {
+    const user = await this._userService.getByEmail(dto.email);
+
+    if (!user)
+      throw new UnauthorizedException(
+        AuthMessageConstants.INCORRECT_CREDENTIALS,
+      );
+
+    const isPasswordValid = await verify(user.password, dto.password);
+
+    if (!isPasswordValid)
+      throw new UnauthorizedException(
+        AuthMessageConstants.INCORRECT_CREDENTIALS,
+      );
+
+    return user;
+  }
+
+  private _getUserWithoutPassword(user: User): AuthResult {
+    const { password, ...userData } = user;
+
+    return userData;
+  }
+}
