52ef48da6e5aa9a0207d5123e4621f78b5df0553
evdmatvey <evd.matvey@mail.ru>
2025-01-07 15:25:56 +0700

    Add auth messages constants

---
+++ src/auth/constants/auth-message.constants.ts
@@ -0,0 +1,9 @@
+export const AuthMessageConstants = {
+  SUCCESS_LOGIN: 'Вы успешно вошли в систему.',
+  SUCCESS_REGISTER: 'Вы успешно зарегистрировались в системе.',
+  SUCCESS_LOGOUT: 'Вы успешно вышли из системы.',
+  USER_NOT_FOUND: 'Пользователь не найден',
+  USER_ALREADY_EXIST: 'Такой пользователь уже есть в системе.',
+  INCORRECT_CREDENTIALS: 'Неверный логин или пароль.',
+  NOT_FOUND_REFRESH_TOKEN: '"Refresh token" не передан в запросе.',
+};
