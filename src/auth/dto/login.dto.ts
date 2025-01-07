871c0f210889c30a1cc0a4d109fb8998d8e81e49
evdmatvey <evd.matvey@mail.ru>
2025-01-07 15:35:13 +0700

    Add swagger desc to dtos

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,12 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 import { AuthValidationConstants } from '../constants/auth-validation.constants';
 
 export class LoginDto {
+  @ApiProperty({ example: 'test@test.test' })
   @IsNotEmpty({ message: AuthValidationConstants.EMPTY_EMAIL })
   @IsString({ message: AuthValidationConstants.IS_STRING_EMAIL })
   @IsEmail({}, { message: AuthValidationConstants.IS_STRING_EMAIL })
   email: string;
 
+  @ApiProperty({ example: '123456' })
   @IsNotEmpty({ message: AuthValidationConstants.EMPTY_PASSWORD })
   @IsString({ message: AuthValidationConstants.IS_STRING_PASSWORD })
   @MinLength(6, { message: AuthValidationConstants.MIN_LENGTH_PASSWORD })
--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,9 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, MinLength, Validate } from 'class-validator';
 import { AuthValidationConstants } from '../constants/auth-validation.constants';
 import { IsPasswordsMatchingConstraint } from '../decorators/is-password-matching-constraint.decorator';
 import { LoginDto } from './login.dto';
 
 export class RegisterDto extends LoginDto {
+  @ApiProperty({ example: '123456' })
   @IsNotEmpty({ message: AuthValidationConstants.EMPTY_PASSWORD_REPEAT })
   @IsString({ message: AuthValidationConstants.IS_STRING_PASSWORD_REPEAT })
   @MinLength(6, { message: AuthValidationConstants.MIN_LENGTH_PASSWORD_REPEAT })
