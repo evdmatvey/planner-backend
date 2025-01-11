cf8e20c160ee78f48013661531ee4483667ba332
evdmatvey <evd.matvey@mail.ru>
2025-01-11 11:55:06 +0700

    Add user controller

---
+++ src/user/user.controller.ts
@@ -0,0 +1,34 @@
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Put,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Auth } from '@/auth/decorators/auth.decorator';
+import { UseUser } from '@/auth/decorators/use-user.decorator';
+import { UserMessageConstants } from './constants/user-message.constants';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UserService } from './user.service';
+
+@Controller('user/profile')
+export class UserController {
+  constructor(private readonly _userService: UserService) {}
+
+  @Put()
+  @Auth()
+  @HttpCode(200)
+  @UsePipes(new ValidationPipe())
+  public async update(
+    @UseUser('id') userId: string,
+    @Body() dto: UpdateUserDto,
+  ) {
+    const { password, ...user } = await this._userService.update(userId, dto);
+
+    return {
+      user,
+      message: UserMessageConstants.SUCCESS_UPDATE,
+    };
+  }
+}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from '@/shared/services/prisma.service';
+import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-  controllers: [],
+  controllers: [UserController],
   providers: [UserService, PrismaService],
   exports: [UserService],
 })
