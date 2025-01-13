dadd06dcfd1b619d8c72366b97e0de7489657e5e
evdmatvey <evd.matvey@mail.ru>
2025-01-13 20:14:57 +0700

    Add tag dto

---
+++ src/tag/dto/update-tag.dto.ts
@@ -0,0 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Color } from '@prisma/__generated__';
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { TagValidationConstants } from '../constants/tag-validation.constants';
+
+export class UpdateTagDto {
+  @ApiProperty({ example: 'Работа' })
+  @IsOptional()
+  @IsString({ message: TagValidationConstants.IS_STRING_TITLE })
+  @IsNotEmpty({ message: TagValidationConstants.EMPTY_TITLE })
+  title: string;
+
+  @ApiProperty({ example: Color.ACCENT })
+  @IsOptional()
+  @IsEnum(Color, { message: TagValidationConstants.INCORRECT_COLOR })
+  @IsString({ message: TagValidationConstants.IS_STRING_COLOR })
+  @IsNotEmpty({ message: TagValidationConstants.EMPTY_COLOR })
+  color?: Color;
+}
