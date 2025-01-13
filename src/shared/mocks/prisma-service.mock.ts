b664cb7e5c1937629330ffb781ccffa4c6a13810
evdmatvey <evd.matvey@mail.ru>
2025-01-13 19:48:39 +0700

    Update prisma mock

--- src/shared/mocks/prisma-service.mock.ts
+++ src/shared/mocks/prisma-service.mock.ts
@@ -7,4 +7,20 @@ export const mockPrismaService = {
     delete: jest.fn(),
     update: jest.fn(),
   },
+  task: {
+    findUnique: jest.fn(),
+    create: jest.fn(),
+    findMany: jest.fn(),
+    findFirst: jest.fn(),
+    delete: jest.fn(),
+    update: jest.fn(),
+  },
+  tag: {
+    findUnique: jest.fn(),
+    create: jest.fn(),
+    findMany: jest.fn(),
+    findFirst: jest.fn(),
+    delete: jest.fn(),
+    update: jest.fn(),
+  },
 };
