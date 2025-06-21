35839aadefe650ede4cc9b7117b152294dcde76b
evdmatvey <evd.matvey@mail.ru>
2025-06-21 19:07:40 +0700

    Add logger to tags controller

--- src/tag/tag.module.ts
+++ src/tag/tag.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { PrismaService } from '@/shared/services/prisma.service';
 import { TagController } from './tag.controller';
 import { TagService } from './tag.service';
 
 @Module({
   controllers: [TagController],
-  providers: [TagService, PrismaService],
+  providers: [TagService, PrismaService, Logger],
   exports: [TagService],
 })
 export class TagModule {}
