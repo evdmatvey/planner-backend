f008d0267c882909e117fe6c787668904881cca2
evdmatvey <evd.matvey@mail.ru>
2025-09-19 21:18:10 +0700

    Remove useless prometheus lib

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 import { TurnstileModule } from 'nestjs-cloudflare-captcha';
 import { AnalyticsModule } from './analytics';
 import { AuthModule } from './auth';
@@ -21,7 +20,6 @@ import { UserModule } from './user';
         process.env.NODE_ENV === 'development' ||
         process.env.NODE_ENV === 'test',
     }),
-    PrometheusModule.register(),
     UserModule,
     AuthModule,
     TaskModule,
